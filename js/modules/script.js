e6abbfb5636c68d290d9632153abe67cef3cc577
VandersonMendes <109023638+VandersonMendes@users.noreply.github.com>
2023-09-25 20:47:56 -0300

    style - projetos - header

--- js/modules/script.js
+++ js/modules/script.js
@@ -6,12 +6,5 @@ import InitScroll from "./InitScroll.js";
 InitScroll()
 import initAtivarDescricaoSkills from "../modules/InitAtivarDescricaoSkills.js";
 initAtivarDescricaoSkills();
-import Slide from "../modules/initSlideStories.js"
-const container = document.getElementById('slide');
-const elements = document.getElementById('slide-elements');
-const controls = document.getElementById('slide-controls');
-if (container && elements && elements.children.length && controls) {
-    const slide = new Slide(container, Array.from(elements.children), controls, 3000);
-    slide.show(0)
-}
+
 
